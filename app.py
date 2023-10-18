5c8c118a5be2d5837ac6a935a7cc930725561491
Atul Kevat <72177710+anonatul@users.noreply.github.com>
2023-10-18 12:28:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["portscanner_database"]
 collection = db["scan_results"]
 
+IMPORTANT_PORTS = [21, 22, 25, 53, 80, 443]  # Modify this list as needed
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,15 +19,25 @@ def index():
 @app.route('/scan', methods=['POST'])
 def scan():
     target = request.form['target']
-    open_ports_details = main(target)
+    start_port = int(request.form.get('startPort', 1))
+    end_port = int(request.form.get('endPort', 1024))
+    open_ports_details = main(target, ports=range(start_port, end_port + 1))
+    
+    os_guess = guess_os(open_ports_details)
 
     # Store results in MongoDB
     collection.insert_one({
         "domain": target,
+        "os_guess": os_guess,
         "results": open_ports_details
     })
 
-    return jsonify({"open_ports_details": open_ports_details})
+    return jsonify({
+        "os_guess": os_guess,
+        "open_ports_details": open_ports_details
+    })
+
+
 
 def get_service_name(port):
     try:
@@ -37,14 +49,32 @@ def get_service_name(port):
 def scan_port(target, port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.5)
+            s.settimeout(0.3)
             s.connect((target, port))
             service_name = get_service_name(port)
             state = "open"
             return port, state, service_name
     except:
-        state = "closed"
-        return port, state, None
+        if port in IMPORTANT_PORTS:
+            state = "closed"
+            return port, state, None
+        return None, None, None  # Skip other ports
+
+def guess_os(open_ports_details):
+    os_guess = "Unknown"
+    
+    # Check for Windows specific ports
+    if any(detail["port"] == "137/tcp" for detail in open_ports_details):
+        os_guess = "Windows"
+    # Check for common Linux ports
+    elif any(detail["port"] == "22/tcp" for detail in open_ports_details):
+        os_guess = "Linux/Unix"
+    
+    elif any(detail["port"] in ["548/tcp", "631/tcp"] for detail in open_ports_details):
+        os_guess = "Mac OS X"
+
+
+    return os_guess
 
 def main(target, ports=range(1, 1025)):
     open_ports_details = []
@@ -53,7 +83,7 @@ def main(target, ports=range(1, 1025)):
         
         for future in futures:
             port, state, service = future.result()
-            if state == "open":
+            if port and state:  # Only consider open ports or "important" closed ones
                 port_info = {
                     "port": f"{port}/tcp",
                     "state": state,
